a7a7265a4620b219882cbcecff1d569d69269a60
JonghyunKim <JonghyunKim>
2016-01-06 15:13:34 +0900

    UIImagePickerControllerを実装

--- KCloth/ViewController.swift
+++ KCloth/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func selectImageButtonTapped(sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
-
-
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let imageview : UIImageView = UIImageView(image: pickedImage)
+            imageview.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            imageview.contentMode = .ScaleAspectFit
+            
+            let gesture = UITapGestureRecognizer(target: self, action: "imageViewSelected:")
+            imageview.addGestureRecognizer(gesture)
+            
+            self.view .bringSubviewToFront(imageview)
+            self.view .addSubview(imageview)
+            
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imageViewSelected(tap: UITapGestureRecognizer) {
+        print("imageViewSelected : \(tap)")
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let currentPoint = touch.locationInView(self.view)
+            print("currentPoint : \(currentPoint.x) \(currentPoint.y)")
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let currentPoint = touch.locationInView(self.view)
+            print("currentPoint : \(currentPoint.x) \(currentPoint.y)")
+        }
+    }
 }
 
